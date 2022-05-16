9968318d8e7cac843b96b04411151e0cd23d1f5e
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-05-16 19:59:41 +0530

    refactor: routing setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import {Navbar, Footer} from './Components'
+import {Home, Login, SignUp, Logout, NotFound} from './Pages'
+import Mockman from "mockman-js";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      hello
+
+      <Navbar />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path='/login' element={<Login />} />
+
+        <Route path='/signup' element={<SignUp />} />
+
+        <Route path='/logout' element={<Logout />} />
+
+        <Route path="/testApi" element={<Mockman />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
