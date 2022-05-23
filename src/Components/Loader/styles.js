072b42ab2ae81b4c4ef30c43b05c910fb5135578
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-05-23 22:18:33 +0530

    refactor: added style to Loader

---
+++ src/Components/Loader/styles.js
@@ -0,0 +1,27 @@
+import { Box, styled } from "@mui/material";
+
+export const LoaderContainer = styled(Box)({
+    margin: 'auto',
+    width: '50%',
+    maxWidth: '400px',
+    minWidth: '150px',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    fontSize: '20px',
+    zIndex: 3,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+});
+
+export const LoaderOverlay = styled(Box)({
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#000',
+    opacity: '0.4'
+});
